b6cb4f83f54b6c8853e6801340afedf0816ef1d5
Jacob Nehama <nehamaj31@aol.com>
2017-09-25 22:50:04 -0600

    Adds correctly named files
    Adds the hello world, first 5 fibo numbers and a loop with 1-10 in C language code

---
+++ Theory/startup/hello.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); //prints hello world
+	return EXIT_SUCCESS;
+}
---
+++ Theory/startup/loop.c
@@ -0,0 +1,22 @@
+/*
+ * ints.c
+ *
+ *  Created on: Sep 25, 2017
+ *      Author: jacobnehama
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int i;
+	for (i=1; i<=10; i++) // for loop to grab each # thru 10
+	{
+		printf("\n %d", i); // prints the numbers
+
+	}
+
+}
