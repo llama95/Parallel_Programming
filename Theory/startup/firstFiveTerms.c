b84e97356b4719a9f4d420609cd3418c30b58a7c
Jacob Nehama <nehamaj31@aol.com>
2017-09-25 21:24:30 -0600

    adds program to write first 5 fibo numbers

---
+++ Theory/startup/firstFiveTerms.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int fibonacci(int term);
+int main(){
+    int terms = 5;
+    int counter;
+    for(counter = 0; counter < terms; counter++){
+        printf("%d ", fibonacci(counter));
+    }
+
+    return 0;
+}
+// n , n-1 , n-2, n-3
+int fibonacci(int term){
+    /* cond to exit recursion*/
+    if(term < 2)
+       return term;
+    return fibonacci(term - 1) + fibonacci(term - 2);
+}
