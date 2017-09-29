834237df8c9bdd891d7014e7cdb365e5d074314b
Jacob Nehama <nehamaj31@aol.com>
2017-09-28 19:08:52 -0600

    Debugged c programs for the parallel debug hw

---
+++ Theory/failure modes/deadlocks.c
@@ -0,0 +1,65 @@
+/*
+ * deadlocks.c
+ *
+ *  Created on: Sep 28, 2017
+ *      Author: jacobnehama
+ */
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+omp_lock_t lockA;
+omp_lock_t lockB;
+int A=1;
+int B=4;
+
+void proc_a() {
+    int x,y;
+    // lock a so that the value won't change unexpectedly
+    omp_set_lock(&lockA);
+    x = A;
+    x = x*2;
+    A = x;
+    omp_unset_lock(&lockA);  //unset lock A
+    // lock b during modification
+    omp_set_lock(&lockB);
+    y = B;
+    y -= x;
+    B = y;
+// unset lock B
+    omp_unset_lock(&lockB);
+}
+
+void proc_b() {
+    int x,y;
+    // lock b so that the value won't change unexpectedly
+    omp_set_lock(&lockB);
+    x = B;
+    x = x/2;
+    B = x;
+    omp_unset_lock(&lockB); // unlock b so a isnt waiting around
+    omp_set_lock(&lockA);
+    y = A;
+    y += x;
+    A = y;
+    omp_unset_lock(&lockA);
+}
+
+int main(int argc, char** argv) {
+    omp_init_lock(&lockA);
+    omp_init_lock(&lockB);
+    #pragma omp parallel num_threads(8)
+    {
+        while(1) {
+            int x=rand();
+            printf("."); fflush(stdout);
+            if(x%2==0) {
+                proc_a();
+            } else {
+                proc_b();
+            }
+        }
+    }
+    return 0;
+}
+
