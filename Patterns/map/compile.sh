dc6fa5100f3e233aca3eb0c79d3231b01c787756
Jacob Nehama <nehamaj31@aol.com>
2017-10-03 23:49:25 -0600

    make sure to use cilk2.c when compiling
    its changed in the script but if youre just running the default that was given to everyone

--- Patterns/map/compile.sh
+++ Patterns/map/compile.sh
@@ -7,7 +7,7 @@ gcc -o recover_pins_serial serial.c -lcrypto -lm
 gcc -o recover_pins_openmp -fopenmp openmp.c -lcrypto
 
 # Compile the cilkplus version
-gcc -o recover_pins_cilk -fcilkplus cilk.c -lcrypto
+gcc -o recover_pins_cilk -fcilkplus cilk2.c -lcrypto
 
 # Compile the tbb version
 g++ -o recover_pins_tbb tbb.c -ltbb
