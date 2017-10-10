e80e6fb684c2b9a2b840159aa2b8a66ea2f6fe11
Jacob Nehama <nehamaj31@aol.com>
2017-10-09 23:37:55 -0600

    Add files via upload
    Speedup on cilk only although they both implement parallelism

---
+++ Patterns/forkjoin/cilk.c
@@ -0,0 +1,44 @@
+/**
+ * Program to scale and project data into 2D and find a centroid
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <cilk/cilk.h>
+
+
+long q(long n) {
+    if(n<3) {
+        return 1;
+    }
+    long q_par;
+    long q_par2;
+    q_par = cilk_spawn q(n-2);
+    q_par2 = q(n - q(n-1));
+    cilk_sync;
+          
+    return q_par2 + q(n-(q_par));
+}
+
+int main(int argc, char** argv) {
+    long out;
+    if(argc!=2) {
+        printf("Usage: %s <integer>\nExiting...\n",argv[0]);
+    }
+    long n = strtol(argv[1],NULL,10);
+
+    struct timespec start_time;
+    struct timespec end_time;
+    clock_gettime(CLOCK_MONOTONIC,&start_time);
+    out = q(n);
+    clock_gettime(CLOCK_MONOTONIC,&end_time);
+    long msec = (end_time.tv_sec - start_time.tv_sec)*1000 + (end_time.tv_nsec - start_time.tv_nsec)/1000000;
+
+    // Print output
+    printf("q(%d) = %d\n",n,out);
+    printf("found in %dms\n",msec);
+
+    return EXIT_SUCCESS;
+}
